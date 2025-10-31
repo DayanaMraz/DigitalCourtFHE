b5fae9feee9fe062d2debeeebd86ffeadd517f1d
DayanaMraz <156245170+DayanaMraz@users.noreply.github.com>
2025-10-30 23:35:51 -0400

    Add files via upload

---
+++ .next/static/gK-YQxu4HJhgKLEQ2St45/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/970-d06ccf284946cbd0.js","static/chunks/pages/index-38b83e3292ce8ae2.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
